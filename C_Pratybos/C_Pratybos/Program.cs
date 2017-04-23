29c331fa9db20926abe9107c6dbcdb58eb57842d
Paulius <paulius75@hotmail.com>
2017-04-24 01:17:40 +0300

    C_darbai

---
+++ C_Pratybos/C_Pratybos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Pratybos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ar dangus yra giedras?");
+            var oras = Convert.ToInt32(Console.ReadLine());
+            rezultatas(oras);
+        }
+        public static void rezultatas(int reiksme)
+        {
+            Console.WriteLine("Įvesta reikšmė {0}.",reiksme);
+            if (reiksme == 0)
+            {
+                Console.WriteLine("Oras yra apsiniaukęs");
+            }
+            else if (reiksme == 1)
+            {
+                Console.WriteLine("Dangus yra giedras");
+            }
+            else
+            {
+                Console.WriteLine("Nėra tokio pasirinkimo.");
+            }
+        }
+
+    }
+}
---
+++ C_Pratybos/C_Pratybos/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("C_Pratybos")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("C_Pratybos")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e2eb2a56-3439-4be3-bf71-5c241d4be6f9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
